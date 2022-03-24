6d5d1b99d1a6971b9df0ab12db73839b3f6fbf2b
Julian Knab <julian.knab@gmail.com>
2022-03-24 20:33:52 +0100

    next

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const HID = require('node-hid');
 
 const devicePath = '/dev/hidraw1';
 const readCommand = [0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00];
+// [1, 128, 51, 1, 0, 0, 0, 0]
 
 readTemperature = (callback) => {
 
@@ -12,20 +13,20 @@ readTemperature = (callback) => {
         if (err) {
             callback.call(NaN, NaN);
         } else {
-            callback.call(toDegreeCelsius(res[2], res[3]), toDegreeCelsius(res[4], res[5]));
+            callback.call(toDegreeCelsius(res[2], res[3]));
         }
     });
 }
 
 toDegreeCelsius = (hiByte, loByte) => {
 
-    if (hiByte === 255 && loByte === 255) {
-        return NaN;
-    }
-    if ((hiByte & 128) === 128) {
-        return -((256-hiByte) + (1 + ~(loByte >> 4)) / 16.0);
-    }
-    return hiByte + ((loByte >> 4) / 16.0);
+    let sign = hiByte & (1 << 7);
+    let temp = ((hiByte & 0x7F) << 8) | loByte;
+
+    if (sign) temp = -temp;
+
+    // Calculate it
+    return temp * 125.0 / 32000.0;
 }
 
 readTemperature((int, ext) => {
