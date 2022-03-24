76a4f31993c13b0eb2ea5660125c34d67f76d54d
Julian Knab <julian.knab@gmail.com>
2022-03-24 20:17:12 +0100

    next

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const temper1 = require('./temper1');
+const HID = require('node-hid');
 
-const temperature = temper1.readTemperature(callback);
+const devicePath = '/dev/hidraw1';
+const readCommand = [0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00];
 
-function callback(int, ext) {
+readTemperature = (callback) => {
+
+    let device = new HID.HID(devicePath);
+    device.write(readCommand);
+    device.read((err, res) => {
+        device.close();
+        if (err) {
+            callback.call(NaN, NaN);
+        } else {
+            callback.call(toDegreeCelsius(res[2], res[3]), toDegreeCelsius(res[4], res[5]));
+        }
+    });
+}
+
+toDegreeCelsius = (hiByte, loByte) => {
+
+    if (hiByte === 255 && loByte === 255) {
+        return NaN;
+    }
+    if ((hiByte & 128) === 128) {
+        return -((256-hiByte) + (1 + ~(loByte >> 4)) / 16.0);
+    }
+    return hiByte + ((loByte >> 4) / 16.0);
+}
+
+readTemperature((int, ext) => {
   console.log('temperature int:', int);
   console.log('temperature ext:', ext);
-}
+});
